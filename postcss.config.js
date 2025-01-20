0c01de31a04ce02a14a24c58899fa83c03ac0a39
benmelz <ben@melz.me>
2025-01-20 13:28:47 -0500

    use scss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+import postcssSass from "@csstools/postcss-sass";
+import autoprefixer from "autoprefixer";
+import postcssScss from "postcss-scss";
+
+export default {
+  parser: postcssScss,
   plugins: [
-    require('postcss-import'),
-    require('postcss-nesting'),
-    require('autoprefixer'),
+    autoprefixer,
+    postcssSass(),
   ],
 }
